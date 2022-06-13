d0f2da5d5bdb9eb426d6ef6ac0f420332b05f7f7
Owenbernard <80883159+Owenbernard@users.noreply.github.com>
2022-06-13 09:45:55 +0200

    Affichage de fenetre

--- Jeu dechec/BirchEngine/Src/main.cpp
+++ Jeu dechec/BirchEngine/Src/main.cpp
@@ -1,45 +1,26 @@
 #include "SDL.h"
-#include "Game.h"
-
-Game *game = nullptr;
+#include <stdbool.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-
-	//fps
-	const int FPS = 60;
-	//limite par seconde
-	const int frameDelay = 1000 / FPS;
-
-	Uint32 frameStart;
-	int frameTime;
-
-	//creation d'un jeu
-	game = new Game();
-	//mise en place de la fenetre de jeu,x,y,fullscreen
-	game->init("GameWindow", 800, 640, false);
-
-	while (game->running())
-	{
-
-		//limiteur
-		frameStart = SDL_GetTicks();
-
-		//event 
-		game->handleEvents();
-		//reaffichage
-		game->update();
-		//affichage
-		game->render();
-
-		frameTime = SDL_GetTicks() - frameStart;
-		
-		if (frameDelay > frameTime)
-		{
-			SDL_Delay(frameDelay - frameTime);
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+	window = SDL_CreateWindow("SDL2_Video", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1000, 1000, SDL_WINDOW_OPENGL);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	bool running = true;
+	SDL_Event event;
+	while (running) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				running = false;
+			}
 		}
+		SDL_RenderClear(renderer);
+		SDL_RenderPresent(renderer);
 	}
-
-	game->clean();
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
 	return 0;
 }
